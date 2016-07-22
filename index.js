31333c770d18c28d56d3e22f92888dbabcf6a0dc
Cheng Zhu <cheng@weteachme.com>
2016-07-22 16:08:18 +1000

    fix dependency issue

--- index.js
+++ index.js
@@ -2,5 +2,19 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-stripe-payment'
+  name: 'ember-stripe-payment',
+  included: function(app) {
+    this._super.included.apply(this, arguments);
+
+    // see: https://github.com/ember-cli/ember-cli/issues/3718
+    while (typeof app.import !== 'function' && app.app) {
+      app = app.app;
+    }
+
+    this.app = app;
+
+    app.import(app.bowerDirectory + '/jquery.payment/lib/jquery.payment.min.js');
+
+    return app;
+  }
 };
